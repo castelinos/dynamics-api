f4c1f1fa85cf271111dd8f0048d4abe6c1602b69
Ben <7ben7ben@gmail.com>
2024-03-31 20:02:23 +0300

    added log

--- Endpoints/index.js
+++ Endpoints/index.js
@@ -32,6 +32,8 @@ router.post('/login', async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
 
+    console.log("user: ", user, " is logging in!");
+
     res.json({ message: 'Login successful' });
   } catch (error) {
     res.status(500).json({ message: error.message });
